c9f91b9d0fe354151976ea2eaa8097ce6dff9b8b
lowjh <114222404+lowjh@users.noreply.github.com>
2024-05-16 03:02:38 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e3db1daad8d24ec2878846c6f2804320',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '字里行间(＾◡＾)✎',
+  domain: 'https://giant-zoo-676.notion.site/e3db1daad8d24ec2878846c6f2804320?pvs=4',
+  author: 'louwh',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',

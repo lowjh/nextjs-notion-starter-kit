3fb662b79c78ea9120cefc627183ccbb9ab26a7a
lowjh <114222404+lowjh@users.noreply.github.com>
2026-05-15 04:25:25 +0800

    fix: 更新正确的 Notion Page ID

--- site.config.ts
+++ site.config.ts
@@ -1,8 +1,8 @@
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
-  rootNotionPageId: 'fa16b37ea19c5c91ac0565f658fdadd8',
-  rootNotionSpaceId: null,
+  rootNotionPageId: 'e3db1daa-d8d2-4ec2-8788-46c6f2804320',
+  rootNotionSpaceId: '7629a35a-5c47-4619-ad27-55a47db5c986',
   name: '字里行间(＾◡＾)✎',
   domain: 'louwh.qzz.io',
   author: 'WenJun·lon',
